4b605d8e110b5394018f3550ec23c042b5b4bea4
Shakthi <sgg329@nyu.edu>
2020-10-20 19:44:05 -0400

    API key setup

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,6 +1,20 @@
-import React from 'react';
 
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 function Home(){
+    const [summonerData,setsummonerData] = useState({});
+    const summonerName = "Areum Fanny";
+    useEffect(()=>{
+        axios
+        .get(`https://cors-anywhere.herokuapp.com/https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${process.env.REACT_APP_API_KEY}`)
+        .then(function(response){
+            setsummonerData(response.data);
+
+        })
+        .catch(function(error){
+            console.warn(error);
+        })
+    },[]);
     return(
         <div>
             <h1>Home</h1>
